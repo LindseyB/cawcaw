9f0e36c94d353129cc05680b7b7a28250456fb51
LindseyB <hazardous.narayan@gmail.com>
2012-02-28 09:49:52 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cawcaw::Application.config.secret_token = 'd1c209a4ebc7bebd1102ac8e49559a5148af3dec2665b9c3de1dd818aafd7ab33910308955f5e19343cc6e6555d9b3d855186420bbbf9d7e1584bdea4ff521a9'
