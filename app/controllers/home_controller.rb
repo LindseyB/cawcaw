c2f24d51dea5136a5594b62cadd05bc46ec13e20
LindseyB <hazardous.narayan@gmail.com>
2012-02-29 16:39:25 -0500

    Homepage now displays the entries.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @entries = Entry.paginate(:page => params[:page])
+    @entries = Entry.paginate(:page => params[:page], :per_page => 10)
   end
 end
