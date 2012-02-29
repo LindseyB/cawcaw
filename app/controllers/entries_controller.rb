317ace961869f3d71e40aa80d8fb0fb667c4c65f
LindseyB <hazardous.narayan@gmail.com>
2012-02-29 17:00:48 -0500

    Added form validation.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -43,15 +43,15 @@ class EntriesController < ApplicationController
   def create
     @entry = Entry.create(params[:entry])
 
-    #respond_to do |format|
-    #  if @entry.save
-    #    format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
-    #    format.json { render json: @entry, status: :created, location: @entry }
-    #  else
-    #    format.html { render action: "new" }
-    #    format.json { render json: @entry.errors, status: :unprocessable_entity }
-    #  end
-    #end
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to @entry, notice: 'Entry was successfully created.' }
+        format.json { render json: @entry, status: :created, location: @entry }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # PUT /entries/1
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,7 @@
 class Entry < ActiveRecord::Base
   has_attached_file :image, :styles => { :large => "800x600", :medium => "400x300#", :thumb => "80x80#" } 
+  validates_presence_of :name
+  validates_attachment_presence :image
+  validates_attachment_content_type :image, 
+                                    :content_type => [ 'image/jpeg', 'image/png' ] 
 end
