6ac89749640d7646ffe90a0e41eb826819c21cb0
LindseyB <hazardous.narayan@gmail.com>
2012-03-02 15:32:23 -0500

    It's dumb to try and reverse something if it isn't the entire list.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @entries = Entry.paginate(:page => params[:page], :per_page => 10)
+    @entries = Entry.paginate(:page => params[:page], :per_page => 10, :order => "created_at ASC")
   end
 
   # GET /details/1
