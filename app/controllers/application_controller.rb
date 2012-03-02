8851f42ef719bb4075387afe9896993fcae7c85f
LindseyB <hazardous.narayan@gmail.com>
2012-03-02 13:31:21 -0500

    Updating how the username and password works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
 
   protected
     def authenticate
-      authenticate_or_request_with_http_basic do |username, password|
-        # TODO: change this
-        username == "admin" && password == "password"
+      if APP_CONFIG['perform_authentication']
+        authenticate_or_request_with_http_basic do |username, password|
+          username == APP_CONFIG['username'] && password == APP_CONFIG['password']
+        end
       end
     end
 end
